1a255257fe4546abdd7e77cf998166c47a522aa1
Eli <1162782792@qq.com>
2023-02-27 00:01:22 +0800

    fix<project>:fix project struct

---
+++ APP/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "sys.h"
+
+
+
+
+int main(void)
+{	
+
+	while(1)
+	{
+
+	}
+}
+
--- OBJ/main.d
+++ OBJ/main.d
@@ -1,5 +1,5 @@
-..\obj\main.o: main.c
-..\obj\main.o: ..\HARDWARE\LED\led.h
+..\obj\main.o: ..\APP\main.c
+..\obj\main.o: ..\APP\main.h
 ..\obj\main.o: ..\SYSTEM\sys\sys.h
 ..\obj\main.o: ..\USER\stm32f10x.h
 ..\obj\main.o: ..\CORE\core_cm3.h
@@ -11,4 +11,3 @@
 ..\obj\main.o: ..\STM32F10x_FWLib\inc\stm32f10x_rcc.h
 ..\obj\main.o: ..\STM32F10x_FWLib\inc\stm32f10x_usart.h
 ..\obj\main.o: ..\STM32F10x_FWLib\inc\misc.h
-..\obj\main.o: ..\SYSTEM\delay\delay.h
